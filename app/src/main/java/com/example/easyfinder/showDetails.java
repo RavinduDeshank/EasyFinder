c862d4e7f3f52a8f461be60c52ee5bfb219f7ead
RavinduDeshank <ravindudeshankwithana@gmail.com>
2020-03-31 13:20:18 +0530

    East Finder Assignment Project

---
+++ app/src/main/java/com/example/easyfinder/showDetails.java
@@ -0,0 +1,14 @@
+package com.example.easyfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_details);
+    }
+}
